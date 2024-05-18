dc0b14f2f1106db2594c7fbdccf75229f19ca007
nazishrehman95 <156924657+nazishrehman95@users.noreply.github.com>
2024-05-19 00:06:49 +0500

    Add files via upload

---
+++ chap4.js
@@ -0,0 +1,25 @@
+//Question#1
+const firstName = "John", lastName = "Doe", age = 30;
+
+//Question#2
+// Legal variable names
+let myVar;
+let _variable;
+let $price;
+let camelCase;
+let myName;
+
+// Illegal variable names
+// let 123variable; // Starts with a number
+// let my-variable; // Contains a hyphen
+// let my variable; // Contains a space
+// let var; // Reserved keyword
+// let @myVariable; // Contains special character '@'
+
+
+//Question#3
+document.write("<h1>Rules for naming JS variables</h1>");
+document.write("<p>Variable names can only contain letters, numbers, dollar signs ($), and underscores (_). For example: $my_1stVariable</p>");
+document.write("<p>Variables must begin with a letter, dollar sign ($), or underscore (_). For example: $name, _name, or name</p>");
+document.write("<p>Variable names are case sensitive.</p>");
+document.write("<p>Variable names should not be JS keywords.</p>");
